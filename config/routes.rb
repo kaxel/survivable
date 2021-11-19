1f9eac971ef13197a31e8547556544faf6548bf6
Krister Axel <ka@ashland.io>
2021-11-19 12:02:46 -0500

    collection for event selection

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :days
   resources :current_games
   resources :events
-  root to: 'events#index'
+  root to: 'days#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
