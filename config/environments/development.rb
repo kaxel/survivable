23cf754843f741e860ed842280b09ca94b8434bc
Krister Axel <ka@ashland.io>
2021-11-20 12:56:17 -0500

    move dev emails to local only

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,15 +75,7 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   
   config.action_mailer.default_url_options = { host: "localhost:3000" }
-  
   config.action_mailer.delivery_method = :smtp
-      config.action_mailer.smtp_settings = {
-        user_name:      "chillfiltr@gmail.com",
-        password:       "FGhjfHJt7776d#2",
-        domain:         ENV['localhost:3000'],
-        address:       'smtp.gmail.com',
-        port:          '587',
-        authentication: :plain,
-        enable_starttls_auto: true
-      }
+  host = 'localhost:3000'
+  config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
 end
