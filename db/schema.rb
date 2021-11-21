843a1413955f6ebcb3dfa4f3be4bd8da8ec13e12
Krister Axel <ka@ashland.io>
2021-11-20 23:25:45 -0500

    add devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_21_013431) do
+ActiveRecord::Schema.define(version: 2021_11_21_042131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,9 +21,7 @@ ActiveRecord::Schema.define(version: 2021_11_21_013431) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "survivalist_id", null: false
-    t.bigint "user_id", null: false
     t.index ["survivalist_id"], name: "index_current_games_on_survivalist_id"
-    t.index ["user_id"], name: "index_current_games_on_user_id"
   end
 
   create_table "days", force: :cascade do |t|
@@ -58,15 +56,16 @@ ActiveRecord::Schema.define(version: 2021_11_21_013431) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", null: false
-    t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "current_games", "survivalists"
-  add_foreign_key "current_games", "users"
   add_foreign_key "days", "current_games"
   add_foreign_key "days", "events"
   add_foreign_key "events", "current_games"
