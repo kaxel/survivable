3e71dc084a70910345c84cd171689e9d135cf2eb
Krister Axel <ka@ashland.io>
2021-11-23 22:32:06 -0500

    animals join table is ready

---
+++ db/migrate/20211124033151_create_join_table_animals_locations.rb
@@ -0,0 +1,8 @@
+class CreateJoinTableAnimalsLocations < ActiveRecord::Migration[6.1]
+  def change
+    create_join_table :animals, :locations do |t|
+      # t.index [:animal_id, :location_id]
+      # t.index [:location_id, :animal_id]
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_24_032204) do
+ActiveRecord::Schema.define(version: 2021_11_24_033151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,11 @@ ActiveRecord::Schema.define(version: 2021_11_24_032204) do
     t.integer "difficulty", limit: 2
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "animals_locations", id: false, force: :cascade do |t|
+    t.bigint "animal_id", null: false
+    t.bigint "location_id", null: false
   end
 
   create_table "climates", force: :cascade do |t|
